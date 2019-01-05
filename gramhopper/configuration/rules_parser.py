bc69986b30388037c38b60c6a3688e1c6b0913a2
Or Bin <orbin50@gmail.com>
2019-01-05 21:58:23 +0200

    #16 Implemented boolean algebra parsing for configuration

--- gramhopper/configuration/rules_parser.py
+++ gramhopper/configuration/rules_parser.py
@@ -17,14 +17,15 @@ class RulesParser:
 
     def __init__(self):
         self.yaml = YAML()
+        self.algebra = boolean.BooleanAlgebra()
         self.global_triggers = {}
         self.global_responses = {}
-        self.trigger_params = RulesParser.TriggerOrResponseParams(key='trigger',
-                                                                  globals=self.global_triggers,
-                                                                  parser=TriggerParser)
-        self.response_params = RulesParser.TriggerOrResponseParams(key='response',
-                                                                   globals=self.global_responses,
-                                                                   parser=ResponseParser)
+        self.trigger_params = TriggerResponseParams(key='trigger',
+                                                    globals=self.global_triggers,
+                                                    parser=TriggerParser)
+        self.response_params = TriggerResponseParams(key='response',
+                                                     globals=self.global_responses,
+                                                     parser=ResponseParser)
 
     def parse_globals(self, config: CommentedMap):
         if 'triggers' in config:
@@ -33,10 +34,37 @@ class RulesParser:
         if 'responses' in config:
             self.global_responses.update(ResponseParser.parse_many(config['responses']))
 
-    def _parse_rule_trigger_or_response(self, rule: CommentedMap, params: TriggerOrResponseParams):
-        # If the trigger/response here is just a name, look for it in the globals
+    def _parse_and_or_expression(self,
+                                 expr: Union[boolean.AND, boolean.OR],
+                                 merge_function: MergeFunction,
+                                 params: TriggerResponseParams) -> TriggerOrResponse:
+        args = list(expr.args)
+        merged = self._parse_boolean_expression(args.pop(0), params)
+        for arg in args:
+            other = self._parse_boolean_expression(arg, params)
+            merged = merge_function(merged, other)
+        return merged
+
+    def _parse_boolean_expression(self, expr: boolean.Expression, params: TriggerResponseParams) -> TriggerOrResponse:
+        if isinstance(expr, boolean.AND):
+            return self._parse_and_or_expression(expr, lambda merged, other: merged & other, params)
+        elif isinstance(expr, boolean.OR):
+            return self._parse_and_or_expression(expr, lambda merged, other: merged | other, params)
+        elif isinstance(expr, boolean.NOT):
+            return ~self._parse_boolean_expression(expr.args[0], params)
+        elif isinstance(expr, boolean.Symbol):
+            return params.globals[str(expr)]
+
+    def _parse_rule_trigger_or_response(self,
+                                        rule: CommentedMap,
+                                        params: TriggerResponseParams) -> TriggerOrResponse:
         if isinstance(rule[params.key], str):
-            return params.globals[rule[params.key]]
+            parsed_expr = self.algebra.parse(rule[params.key])
+            if isinstance(parsed_expr, boolean.Symbol):
+                # If the trigger/response here is just a name, look for it in the globals
+                return params.globals[rule[params.key]]
+            else:
+                return self._parse_boolean_expression(parsed_expr, params)
 
         return params.parser.parse_single(rule[params.key])
 
