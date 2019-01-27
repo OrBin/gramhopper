0ad617248a43b239de9b278abd32b433f62e91c9
Or Bin <orbin50@gmail.com>
2019-01-27 23:25:57 +0200

    #17 (WIP) Generalized handling of parameters which are triggers themselves

--- gramhopper/configuration/triggers_reponses_parsers.py
+++ gramhopper/configuration/triggers_reponses_parsers.py
@@ -1,4 +1,7 @@
 import abc
+import typing
+from inspect import isclass
+from gramhopper.triggers.basic_triggers import BaseTrigger
 from ..responses import Responses
 from ..triggers import Triggers
 
@@ -42,13 +45,37 @@ class TriggerParser(BaseParser):
 
         config_copy = dict(config)
 
+        trigger_class = Triggers[config['type']]
+
         if config['type'] == Triggers.event_streak.name:
-            if 'counting_event_trigger' in config:
-                config_copy['counting_event_trigger'] = cls.parse_single(config['counting_event_trigger'], globals)
 
-            if 'resetting_event_trigger' in config:
-                config_copy['resetting_event_trigger'] = cls.parse_single(config['resetting_event_trigger'], globals)
 
+            parameters_type_hints = typing.get_type_hints(trigger_class.__init__)
+            config.pop('type')
+
+            for parameter_name in config:
+
+                # Meta-parameters to ignore
+                if parameter_name in ['type', 'name']:
+                    continue
+
+                should_parse_parameter = False
+                type_hint = parameters_type_hints[parameter_name]
+                origin_type = getattr(type_hint, '__origin__', None)
+
+                if origin_type is not None:
+                    if origin_type == typing.Union:
+                        if BaseTrigger in type_hint.__args__:
+                            should_parse_parameter = True
+                    else:
+                        raise NotImplementedError(f'Origin type {origin_type} is currently not supported')
+
+                elif isclass(type_hint) and issubclass(type_hint, BaseTrigger):
+                    should_parse_parameter = True
+
+                if should_parse_parameter:
+                    print(parameter_name)
+                    config_copy[parameter_name] = cls.parse_single(config[parameter_name], globals)
 
         return super().parse_single(config_copy, globals)
 
