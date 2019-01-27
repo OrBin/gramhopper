7286f27400ab4111ee30775199cc2f6b076dd347
Or Bin <orbin50@gmail.com>
2019-01-27 20:13:03 +0200

    Merge branch 'master' of /cloud/Dropbox/Programming/code/python/gramhopper with conflicts.

--- gramhopper/configuration/rules_parser.py
+++ gramhopper/configuration/rules_parser.py
@@ -1,45 +1,28 @@
-from typing import NamedTuple, Dict, Type
 from ruamel.yaml import YAML
 from ruamel_yaml.comments import CommentedMap
+
+from .params_manager import ParamsManager
+from .rules_parsing_helper import RulesParsingHelper
 from ..handlers.handler import Handler
-from .triggers_reponses_parsers import TriggerParser, ResponseParser, BaseParser
+#from .trigger_response_params import TriggerParams, ResponseParams
 
 
 class RulesParser:
 
-    class TriggerOrResponseParams(NamedTuple):
-        key: str
-        globals: Dict
-        parser: Type[BaseParser]
-
     def __init__(self):
         self.yaml = YAML()
-        self.global_triggers = {}
-        self.global_responses = {}
-        self.trigger_params = RulesParser.TriggerOrResponseParams(key='trigger',
-                                                                  globals=self.global_triggers,
-                                                                  parser=TriggerParser)
-        self.response_params = RulesParser.TriggerOrResponseParams(key='response',
-                                                                   globals=self.global_responses,
-                                                                   parser=ResponseParser)
+        #self.global_triggers = {}
+        #self.global_responses = {}
+        self.trigger_params = ParamsManager.get_trigger_params() # TriggerParams(globals={})
+        self.response_params = ParamsManager.get_response_params() # ResponseParams(globals={})
 
     def parse_globals(self, config: CommentedMap):
-        if 'triggers' in config:
-            self.global_triggers.update(TriggerParser.parse_many(config['triggers']))
-
-        if 'responses' in config:
-            self.global_responses.update(ResponseParser.parse_many(config['responses']))
-
-    def _parse_rule_trigger_or_response(self, rule: CommentedMap, params: TriggerOrResponseParams):
-        # If the trigger/response here is just a name, look for it in the globals
-        if isinstance(rule[params.key], str):
-            return params.globals[rule[params.key]]
-
-        return params.parser.parse_single(rule[params.key])
+        RulesParsingHelper.add_globals(config, self.trigger_params)
+        RulesParsingHelper.add_globals(config, self.response_params)
 
     def parse_single_rule(self, rule: CommentedMap):
-        trigger = self._parse_rule_trigger_or_response(rule, self.trigger_params)
-        response = self._parse_rule_trigger_or_response(rule, self.response_params)
+        trigger = RulesParsingHelper.parse_rule_trigger_or_response(rule, self.trigger_params)
+        response = RulesParsingHelper.parse_rule_trigger_or_response(rule, self.response_params)
         probability = rule['probability'] if 'probability' in rule else 1
         return Handler(trigger, response, probability=probability)
 
