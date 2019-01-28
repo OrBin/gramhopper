d5c1be52d662b15e756f9722785c41306e9aaae0
Or Bin <orbin50@gmail.com>
2019-01-28 07:43:38 +0200

    #17 Extracted the code that checks whether a parameter should be parsed as a trigger to a function

--- gramhopper/configuration/triggers_reponses_parsers.py
+++ gramhopper/configuration/triggers_reponses_parsers.py
@@ -39,16 +39,11 @@ class TriggerParser(BaseParser):
         return Triggers
 
     @classmethod
-    def parse_single(cls, config, globals):
-        if isinstance(config, str):
-            return globals[config]
-
-        config_copy = dict(config)
-
+    def parameters_to_parse_as_trigger(cls, config):
         trigger_class = Triggers[config['type']]
-
         parameters_type_hints = typing.get_type_hints(trigger_class.__init__)
-        config.pop('type')
+
+        parameters_to_parse = []
 
         for parameter_name in config:
 
@@ -59,22 +54,34 @@ class TriggerParser(BaseParser):
             # If this parameter has not type hint, we cannot check if it's a trigger or not
             if parameter_name not in parameters_type_hints:
                 continue
+
             type_hint = parameters_type_hints[parameter_name]
 
-            should_parse_parameter = False
             origin_type = getattr(type_hint, '__origin__', None)
 
             if origin_type is not None:
                 if origin_type == typing.Union:
                     if BaseTrigger in type_hint.__args__:
-                        should_parse_parameter = True
+                        parameters_to_parse.append(parameter_name)
                 else:
                     raise NotImplementedError(f'Origin type {origin_type} is currently not supported')
             elif isclass(type_hint) and issubclass(type_hint, BaseTrigger):
-                should_parse_parameter = True
+                parameters_to_parse.append(parameter_name)
 
-            if should_parse_parameter:
-                config_copy[parameter_name] = cls.parse_single(config[parameter_name], globals)
+        return parameters_to_parse
+
+
+    @classmethod
+    def parse_single(cls, config, globals):
+        if isinstance(config, str):
+            return globals[config]
+
+        config_copy = dict(config)
+        parameters_to_parse = cls.parameters_to_parse_as_trigger(config)
+        config.pop('type')
+
+        for parameter in parameters_to_parse:
+            config_copy[parameter] = cls.parse_single(config[parameter], globals)
 
         return super().parse_single(config_copy, globals)
 
