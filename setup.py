7f56eead4f99d2484b4594b47e66259d20f7e373
Or Bin <orbin50@gmail.com>
2019-01-29 21:50:44 +0200

    Added version number

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='gramhopper',
+    version='1.0',
 
     description='A bot platform for automatic responses based on various triggers',
     long_description=long_description,
