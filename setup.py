57d9a1bbbdd0f841838d8c3ede07c535274176c1
Or Bin <orbin50@gmail.com>
2019-03-20 00:03:19 +0200

    Update to 1.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='gramhopper',
-    version='1.0.5',
+    version='1.0.6',
 
     description='A bot platform for automatic responses based on various triggers',
     long_description=LONG_DESCRIPTION,
