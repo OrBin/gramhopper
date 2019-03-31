27e8acf025a3064010759bd198dc3c74c06eef5a
Or Bin <orbin50@gmail.com>
2019-03-31 08:00:00 +0300

    #10 Generalized fixture for using more updates in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,15 +8,38 @@ from pytest import fixture
 from telegram import Message, User, Chat, Update
 
 
-# This fixture was copied from the tests in python-telegram-bot:
-#
 @fixture(scope='module')
 def update():
     """
     Creates a dummy update fixture.
-    This fixture was copied from the tests in python-telegram-bot:
-    https://github.com/python-telegram-bot/python-telegram-bot/blob/b5891a6/tests/test_filters.py#L29
     :return: a dummy update fixture
     """
-    return Update(0, Message(0, User(0, 'Testuser', False), datetime.now(),
-                             Chat(0, 'private')))
+    return _generate_new_update_impl()
+
+
+@fixture(scope='module')
+def generate_new_update():
+    """
+    Returns a generator function for new updates
+    :return: an updates generator function
+    """
+    return _generate_new_update_impl
+
+
+def _generate_new_update_impl(update_id=0,
+                              message_id=0,
+                              user_id=0,
+                              user_first_name='Testuser',
+                              user_is_bot=False,
+                              message_date=None,
+                              chat_id=0,
+                              chat_type='private'):
+
+    if not message_date:
+        message_date = datetime.now()
+
+    return Update(update_id,
+                  Message(message_id,
+                          User(user_id, user_first_name, user_is_bot),
+                          message_date,
+                          Chat(chat_id, chat_type)))
---
+++ tests/triggers/test_event_streak_trigger.py
@@ -0,0 +1,3 @@
+import pytest
+from ...gramhopper.triggers.event_streak_trigger import EventStreakTrigger
+
