41ee4d773e95c4cb76ec452b88cf237d957d4625
Or Bin <orbin50@gmail.com>
2019-04-03 21:05:03 +0300

    #10 Added fixtures for bot and chat_id

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,19 @@ Read more here: https://docs.pytest.org/en/latest/fixture.html#conftest-py-shari
 """
 
 from datetime import datetime
+from os import getenv
 from pytest import fixture
-from telegram import Message, User, Chat, Update
+from telegram import Message, User, Chat, Update, Bot
+
+
+PUBLIC_TEST_BOT_PARAMETERS = {
+    'token': '796912871:AAEc8dtCAmj4Jf4uht5dMfUWJCqOh8RDvAc',
+    'chat_id': '-240130726',  # This is a dedicated group for testing
+}
 
 
 @fixture(scope='module')
-def update():
+def update() -> Update:
     """
     Creates a dummy update fixture.
     :return: a dummy update fixture
@@ -35,7 +42,7 @@ def _generate_new_update_impl(update_id=0,
                               message_date=None,
                               chat_id=0,
                               chat_type='private',
-                              message_text=None):
+                              message_text=None) -> Update:
 
     if not message_date:
         message_date = datetime.now()
@@ -46,3 +53,31 @@ def _generate_new_update_impl(update_id=0,
                           message_date,
                           Chat(chat_id, chat_type),
                           text=message_text))
+
+
+def _get_bot_parameter(parameter_name: str) -> str:
+    value = getenv(parameter_name.upper())
+
+    if value:
+        return value
+
+    return PUBLIC_TEST_BOT_PARAMETERS[parameter_name]
+
+
+@fixture(scope='module')
+def bot() -> Bot:
+    """
+    Creates a valid bot instance, with a token from 'TOKEN' environment variable if exists,
+    or the default one from PUBLIC_TEST_BOT_PARAMETERS otherwise.
+    :return: a bot instance
+    """
+    return Bot(_get_bot_parameter('token'))
+
+@fixture(scope='module')
+def bot() -> str:
+    """
+    Returns a chat id from 'CHAT_ID' environment variable if exists,
+    or the default one from PUBLIC_TEST_BOT_PARAMETERS otherwise.
+    :return: a chat id
+    """
+    return _get_bot_parameter('chat_id')
