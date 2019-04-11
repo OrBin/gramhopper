a1a12be2702d7c418c99716c6a2aa4f3e8156a74
Or Bin <orbin50@gmail.com>
2019-04-11 14:37:38 +0300

    #10 Added tests for _PresetTextResponse

--- tests/responses/test_preset_responses.py
+++ tests/responses/test_preset_responses.py
@@ -1,8 +1,8 @@
 import time
 import pytest
 from flaky import flaky
-from ...gramhopper.responses.preset_responses import _PresetTextResponse, \
-    _PresetDocumentResponse, _PresetMessageResponse, _PresetReplyResponse
+from ...gramhopper.responses.preset_responses import _PresetDocumentResponse, \
+    _PresetMessageResponse, _PresetReplyResponse
 
 
 FLAKY_MAX_RUNS = 6
@@ -124,3 +124,33 @@ class TestPresetDocumentResponse:
 
     def test_single_preset_reply_with_some_payload(self, bot, bot_chat_id, generate_new_update):
         self._test_preset_document_url(bot, bot_chat_id, generate_new_update, {'key': 'value'})
+
+
+class TestPresetTextResponse:
+
+    SINGLE_PRESET_TEXT = 'one'
+    MULTIPLE_PRESET_TEXTS = ['two', 'three']
+    FLAKY_MAX_RUNS_FOR_RANDOMNESS = 20
+    FLAKY_MIN_PASSES_FOR_RANDOMNESS = 1
+
+    def test_get_response_text_from_single_preset_text(self):
+        # _PresetTextResponse is abstract, so we're using _PresetMessageResponse to test it.
+        response = _PresetMessageResponse(self.SINGLE_PRESET_TEXT)
+        response_text = response.get_response_text()
+        assert response_text == self.SINGLE_PRESET_TEXT
+
+    def _test_from_multiple_texts(self, response, preset_index):
+        response_text = response.get_response_text()
+        assert response_text == self.MULTIPLE_PRESET_TEXTS[preset_index]
+
+    @flaky(max_runs=FLAKY_MAX_RUNS_FOR_RANDOMNESS, min_passes=FLAKY_MIN_PASSES_FOR_RANDOMNESS)
+    def test_get_response_text_from_multiple_preset_texts_first(self):
+        # _PresetTextResponse is abstract, so we're using _PresetMessageResponse to test it.
+        response = _PresetMessageResponse(self.MULTIPLE_PRESET_TEXTS)
+        self._test_from_multiple_texts(response, 0)
+
+    @flaky(max_runs=FLAKY_MAX_RUNS_FOR_RANDOMNESS, min_passes=FLAKY_MIN_PASSES_FOR_RANDOMNESS)
+    def test_get_response_text_from_multiple_preset_texts_second(self):
+        # _PresetTextResponse is abstract, so we're using _PresetMessageResponse to test it.
+        response = _PresetMessageResponse(self.MULTIPLE_PRESET_TEXTS)
+        self._test_from_multiple_texts(response, 1)
