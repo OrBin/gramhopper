9c22159d3c1cd1607dfba3323320991a4be577f8
Or Bin <orbin50@gmail.com>
2019-04-17 23:01:57 +0300

    #11 Fixed line separation

--- gramhopper/paths.py
+++ gramhopper/paths.py
@@ -7,6 +7,7 @@ DEFAULT_RULES_FILE_NAME = 'rules.yml'
 USERS_FILE_NAME = 'users.json'
 LOG_FILE_NAME = 'gramhopper.log'
 
+
 def token_file_path():
     return Path(CONFIG_DIR, TOKEN_FILE_NAME)
 
@@ -17,6 +18,7 @@ def default_rules_file_path():
 
 def users_file_path():
     return Path(CONFIG_DIR, USERS_FILE_NAME)
+
 
 def log_file_path(file_name=LOG_FILE_NAME):
     return Path(CONFIG_DIR, file_name)
