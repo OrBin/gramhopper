ac156bc8089f102801ed0d62d2d953bae893d587
Or Bin <orbin50@gmail.com>
2019-04-21 23:02:42 +0300

    #43 Added publish task (publishing package and docker image)

--- tasks.py
+++ tasks.py
@@ -4,27 +4,27 @@ import sys
 from invoke import task
 
 
-DEFAULT_DOCKER_TAG = 'orbin/gramhopper'
-DOCKER_TAG_ENV_VARIABLE = 'DOCKER_IMAGE_TAG'
+DEFAULT_DOCKER_IMAGE = 'orbin/gramhopper'
+DOCKER_IMAGE_ENV_VARIABLE = 'DOCKER_IMAGE_TAG'
 TASK_SUCCESS_CODE = 0
 TASK_FAILURE_CODE = 1
 
 
 @task
-def build(context, package=True, docker_image=True, docker_tag=DEFAULT_DOCKER_TAG, docs=False):
+def build(context, package=True, docker_image=True, docker_tag=DEFAULT_DOCKER_IMAGE, docs=False):
     if package:
         print('Building package...')
-        context.run('python setup.py sdist bdist_wheel')
+        context.run('python setup.py sdist bdist_wheel', echo=True)
 
     if docker_image:
         print('Building docker image...')
 
         # If not specified the tag as a flag, the tag from the environment variable is preferred
-        if docker_tag == DEFAULT_DOCKER_TAG and \
-                DOCKER_TAG_ENV_VARIABLE in os.environ:
-            docker_tag = os.environ[DOCKER_TAG_ENV_VARIABLE]
+        if docker_tag == DEFAULT_DOCKER_IMAGE and \
+                DOCKER_IMAGE_ENV_VARIABLE in os.environ:
+            docker_tag = os.environ[DOCKER_IMAGE_ENV_VARIABLE]
 
-        context.run(f'docker build -t {docker_tag} .')
+        context.run(f'docker build -t {docker_tag} .', echo=True)
 
     if docs:
         build_docs(context)
@@ -34,7 +34,7 @@ def build(context, package=True, docker_image=True, docker_tag=DEFAULT_DOCKER_TA
 def build_docs(context):
     print('Building docs...')
     with context.cd('docs'):
-        context.run('make html')
+        context.run('make html', echo=True)
 
 
 @task
@@ -44,17 +44,19 @@ def lint(context):
     exit_code = TASK_SUCCESS_CODE
 
     print('Running pylint on source code...')
-    result = context.run(f'pylint {source_code_dirs}', warn=True)
+    result = context.run(f'pylint {source_code_dirs}', warn=True, echo=True)
     if not result.exited == TASK_SUCCESS_CODE:
         exit_code = TASK_FAILURE_CODE
 
     print('Running pylint on test code...')
-    result = context.run(f'pylint --rcfile=./tests/tests.pylintrc {test_code_dirs}', warn=True)
+    result = context.run(f'pylint --rcfile=./tests/tests.pylintrc {test_code_dirs}',
+                         warn=True,
+                         echo=True)
     if not result.exited == TASK_SUCCESS_CODE:
         exit_code = TASK_FAILURE_CODE
 
     print('Running flake8...')
-    result = context.run('flake8 --exclude=venv', warn=True)
+    result = context.run('flake8 --exclude=venv', warn=True, echo=True)
     if not result.exited == TASK_SUCCESS_CODE:
         exit_code = TASK_FAILURE_CODE
 
@@ -63,7 +65,7 @@ def lint(context):
     python_version = '%d.%d' % (sys.version_info.major,  # pytype: disable=attribute-error
                                 sys.version_info.minor)  # pytype: disable=attribute-error
     cmd = f'pytype  --config ./setup.cfg -V {python_version} {source_code_dirs} {test_code_dirs}'
-    result = context.run(cmd, warn=True)
+    result = context.run(cmd, warn=True, echo=True)
     if not result.exited == TASK_SUCCESS_CODE:
         exit_code = TASK_FAILURE_CODE
 
@@ -80,10 +82,29 @@ def test(context):
     context.run('cp -R tests/assets/.gramhopper ~/.gramhopper')  # TODO change to python code
 
     print('Running tests...')
-    context.run('pytest')
+    context.run('pytest', echo=True)
 
     # print('Removing test assets...')
     # context.run('rm ~/.gramhopper')  # TODO change to python code
+
+
+@task
+def publish(context, docker_tag=None, docker_latest=False, production_pypi=False):
+    pypi_type = 'production' if production_pypi else 'test'
+    print(f'Uploading package to {pypi_type} PyPI...')
+    repo_url_flag = '' if production_pypi else '--repository-url https://test.pypi.org/legacy/'
+    context.run(f'twine upload {repo_url_flag} dist/*', echo=True)
+
+    if docker_tag:
+        publish_docker_image(context, f'{DEFAULT_DOCKER_IMAGE}:{docker_tag}')
+
+    if docker_latest:
+        publish_docker_image(context, f'{DEFAULT_DOCKER_IMAGE}:latest')
+
+
+def publish_docker_image(context, full_image_name):
+    print(f'Uploading docker image {full_image_name} to PyPI...')
+    context.run(f'docker push {full_image_name}', echo=True)
 
 
 @task
@@ -132,9 +153,9 @@ def clean_docker(context):
     # If docker is installed, remove both the image with the default tag and
     # the image with the tag from the environment variable
     if docker_result.exited == TASK_SUCCESS_CODE:
-        tags_to_remove = [DEFAULT_DOCKER_TAG]
-        if DOCKER_TAG_ENV_VARIABLE in os.environ:
-            tags_to_remove.append(os.environ[DOCKER_TAG_ENV_VARIABLE])
+        tags_to_remove = [DEFAULT_DOCKER_IMAGE]
+        if DOCKER_IMAGE_ENV_VARIABLE in os.environ:
+            tags_to_remove.append(os.environ[DOCKER_IMAGE_ENV_VARIABLE])
 
         hashes_to_remove = []
         for tag in tags_to_remove:
@@ -145,7 +166,7 @@ def clean_docker(context):
 
         if len(hashes_to_remove):
             hashes_to_remove = ' '.join(set(hashes_to_remove))
-            context.run(f'docker rmi -f {hashes_to_remove}')
+            context.run(f'docker rmi -f {hashes_to_remove}', echo=True)
         else:
             print('No docker images to remove')
     else:
