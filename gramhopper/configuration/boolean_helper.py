72e3169dcd336c5b1f73fd3d7e40bca73f207789
Or Bin <orbin50@gmail.com>
2019-08-13 08:42:47 +0300

    #32 Added docstrings to boolean_helper.py and triggers_responses_parsers.py

--- gramhopper/configuration/boolean_helper.py
+++ gramhopper/configuration/boolean_helper.py
@@ -7,6 +7,9 @@ from .trigger_response import TriggerResponse
 
 
 class BooleanHelper:
+    """
+    A helper class for parsing boolean algebra expressions.
+    """
 
     ParsingFunction = Callable[[Union[CommentedMap, str], GlobalsDict],
                                TriggerResponse]
@@ -14,6 +17,12 @@ class BooleanHelper:
     @staticmethod
     def evaluate_boolean_expression(expr: boolean.Expression,
                                     global_elements: GlobalsDict) -> TriggerResponse:
+        """
+        Convert a boolean expression into a `BaseTrigger`/`BaseResponse` object
+        :param expr: A boolean expression parsed from a subrule (a configuration subtree)
+        :param global_elements: A dictionary with all triggers and responses configured globally
+        :return: The trigger/response object created from the given boolean expression
+        """
         # If the trigger/response here is just a name, look for it in the globals
         if isinstance(expr, boolean.Symbol):
             return global_elements[str(expr)]
@@ -28,6 +37,13 @@ class BooleanHelper:
     def parse_subrule_as_trigger_or_response(subrule: Union[CommentedMap, str],
                                              global_elements: GlobalsDict,
                                              parsing_func: ParsingFunction) -> TriggerResponse:
+        """
+        Convert a subrule (a configuration subtree) into a `BaseTrigger`/`BaseResponse` object
+        :param subrule: A subrule (a configuration subtree)
+        :param global_elements: A dictionary with all triggers and responses configured globally
+        :param parsing_func: A specific parsing function (for the specific subrule type to parse)
+        :return: The trigger/response object created from the given subrule
+        """
         if isinstance(subrule, str):
             algebra = boolean.BooleanAlgebra()
             parsed_expr = algebra.parse(subrule)
--- gramhopper/configuration/triggers_reponses_parsers.py
+++ gramhopper/configuration/triggers_reponses_parsers.py
@@ -1,20 +1,34 @@
 import abc
-import typing
+from typing import Dict, get_type_hints, Union, List
 from inspect import isclass
+from .partial_ruamel_yaml import CommentedMap, CommentedSeq
+from .trigger_response import TriggerResponse
+from .globals_dict import GlobalsDict
 from .boolean_helper import BooleanHelper
+from ..dict_enum import DictEnum
 from ..triggers.basic_triggers import BaseTrigger
 from ..responses import Responses
 from ..triggers import Triggers
 
 
 class BaseParser(abc.ABC):
+    """ A common base parser for trigger/response configuration parsers s"""
+
     @staticmethod
     @abc.abstractmethod
-    def mapping_class():
+    def mapping_class() -> DictEnum:
         """ Returns the mapping class (`Triggers` or `Responses`)"""
 
     @classmethod
-    def parse_single(cls, config, global_elements):  # pylint: disable=unused-argument
+    def parse_single(cls,
+                     config: Dict[str, any],
+                     global_elements: GlobalsDict) -> TriggerResponse:  # pylint: disable=unused-argument
+        """
+        Parse a single configuration subtree as a trigger/response
+        :param config: A configuration subtree to parse
+        :param global_elements: A dictionary with all triggers and responses configured globally
+        :return: The trigger/response object created from the given configuration subtree
+        """
         config_copy = dict(config)
         name = None
         if 'name' in config_copy:
@@ -33,7 +47,15 @@ class BaseParser(abc.ABC):
         return element
 
     @classmethod
-    def parse_many(cls, config, global_elements):
+    def parse_many(cls, config: CommentedSeq, global_elements: GlobalsDict) \
+            -> Dict[str, TriggerResponse]:
+        """
+        Parse all triggers/responses in a configuration
+        :param config: A configuration subtree to config as a list of triggers/responses
+        :param global_elements: A dictionary with all triggers and responses configured globally
+        :return: A dictionary where keys are the original keys from the given configuration subtree
+            and values are triggers/responses
+        """
         return {
             element['name']: cls.parse_single(element, global_elements)
             for element
@@ -42,14 +64,23 @@ class BaseParser(abc.ABC):
 
 
 class TriggerParser(BaseParser):
+    """ A parser for trigger configuration """
+
     @staticmethod
-    def mapping_class():
+    def mapping_class() -> DictEnum:
+        """ Returns the mapping class (`Triggers` or `Responses`)"""
         return Triggers
 
     @classmethod
-    def parameters_to_parse_as_trigger(cls, config):
+    def parameters_to_parse_as_trigger(cls, config: CommentedMap) -> List[str]:
+        """
+        Finds parameters in configuration which should be parsed as triggers themselves (For
+        example, when using a trigger that gets another trigger as a parameter).
+        :param config: A configuration subtree to parse
+        :return: List of the found parameter names
+        """
         trigger_class = Triggers[config['type']]
-        parameters_type_hints = typing.get_type_hints(trigger_class.__init__)
+        parameters_type_hints = get_type_hints(trigger_class.__init__)
         parameters_to_parse = []
 
         for parameter_name in config:
@@ -67,7 +98,7 @@ class TriggerParser(BaseParser):
             origin_type = getattr(type_hint, '__origin__', None)
 
             if origin_type is not None:
-                if origin_type == typing.Union:
+                if origin_type == Union:
                     if BaseTrigger in type_hint.__args__:
                         parameters_to_parse.append(parameter_name)
                 else:
@@ -79,13 +110,27 @@ class TriggerParser(BaseParser):
         return parameters_to_parse
 
     @classmethod
-    def parse_single(cls, config, global_elements):
+    def parse_single(cls, config: Dict[str, any], global_elements: GlobalsDict) \
+            -> BaseTrigger:
+        """
+        Parse a single configuration subtree as a trigger
+        :param config: A configuration subtree to parse
+        :param global_elements: A dictionary with all triggers and responses configured globally
+        :return: The trigger object created from the given configuration subtree
+        """
         return BooleanHelper.parse_subrule_as_trigger_or_response(config,
                                                                   global_elements,
-                                                                  cls.parse_single_recursively)
+                                                                  cls._parse_single_recursively)
 
     @classmethod
-    def parse_single_recursively(cls, config, global_elements):
+    def _parse_single_recursively(cls, config: CommentedMap, global_elements: GlobalsDict) \
+            -> BaseTrigger:
+        """
+        Recursively parse a single configuration subtree as a trigger
+        :param config: A configuration subtree to parse
+        :param global_elements: A dictionary with all triggers and responses configured globally
+        :return: The trigger object created from the given configuration subtree
+        """
         if isinstance(config, str):
             return global_elements[config]
 
@@ -100,6 +145,9 @@ class TriggerParser(BaseParser):
 
 
 class ResponseParser(BaseParser):
+    """ A parser for response configuration """
+
     @staticmethod
-    def mapping_class():
+    def mapping_class() -> DictEnum:
+        """ Returns the mapping class (`Triggers` or `Responses`)"""
         return Responses
