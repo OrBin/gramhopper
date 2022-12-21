87bdf86e822532212d0e2d0d3172c19bc73c532f
Or Bin <orbin50@gmail.com>
2022-12-22 01:01:36 +0200

    Bumped versions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = Path("README.md").read_text(encoding='utf-8')
 
 setup(
     name='gramhopper',
-    version='4.0.3',
+    version='5.0.0',
 
     description='A bot platform for automatic responses based on various triggers',
     long_description=LONG_DESCRIPTION,
